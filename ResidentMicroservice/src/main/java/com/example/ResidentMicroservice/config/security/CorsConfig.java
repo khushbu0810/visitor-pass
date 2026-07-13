3ae9d0dddb038df0f1ab482ae044b1f432866104
Khushbu <Kahlwt08@gmail.com>
2026-07-13 17:52:42 +0530

    added frontend site to resolve CORS issue

--- ResidentMicroservice/src/main/java/com/example/ResidentMicroservice/config/security/CorsConfig.java
+++ ResidentMicroservice/src/main/java/com/example/ResidentMicroservice/config/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", "https://venuepilot.netlify.app")
+                        .allowedOrigins("http://localhost:4200", "https://grand-alpaca-365ab0.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
